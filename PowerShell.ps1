c483f2057c267697a127b706686c9d5fa4f40e16
Naresh1708 <50539116+Naresh1708@users.noreply.github.com>
2019-09-28 14:08:22 -0400

    Update PowerShell.ps1
    updated

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -7,4 +7,6 @@ New-Item -Path 'C:\temp' -ItemType directory
 # Using the environment variables configured above.
 Set-Content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
 
+
+# Using the environment variables configured above.
 Set-Content -Path "E:\Technologies\B18-Devops Course\Powershellproj\$($env:Filename).txt" -Value $env:Message
