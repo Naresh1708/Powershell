d77d3d8d8bf5381a6e3b3e552d8292b87bd750c9
Naresh1708 <50539116+Naresh1708@users.noreply.github.com>
2019-09-28 14:05:13 -0400

    Update PowerShell.ps1
    update to create file in E

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -6,3 +6,5 @@ New-Item -Path 'C:\temp' -ItemType directory
  
 # Using the environment variables configured above.
 Set-Content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
+
+Set-Content -Path "E:\Technologies\B18-Devops Course\Powershellproj\$($env:Filename).txt" -Value $env:Message
